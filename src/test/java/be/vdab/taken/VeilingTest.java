e307aaefc107769b390d363d8f0fef3ec395a4b3
NickVandenbroeck <nick.vandenbroeck@vdabcampus.be>
2025-03-18 15:42:55 +0100

    Test fixtures

--- src/test/java/be/vdab/taken/VeilingTest.java
+++ src/test/java/be/vdab/taken/VeilingTest.java
@@ -1,5 +1,6 @@
 package be.vdab.taken;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -7,22 +8,26 @@ import java.math.BigDecimal;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class VeilingTest {
+    private Veiling veiling;
+
+    @BeforeEach
+    void beforeEach() {
+        veiling = new Veiling();
+    }
+
     @Test
     void hetHoogsteBodVanEenNieuweVeilingIs0€() {
-        var veiling = new Veiling();
         assertThat(veiling.getHoogsteBod()).isZero();
     }
 
     @Test
     void naEenBodVan1000€IsHetHoogsteBod1000€() {
-        var veiling = new Veiling();
         veiling.doeBod(BigDecimal.valueOf(1000));
         assertThat(veiling.getHoogsteBod()).isEqualByComparingTo("1000");
     }
 
     @Test
     void naEenBodVan2000€IsHetHoogsteBod2000€() {
-        var veiling = new Veiling();
         veiling.doeBod(BigDecimal.valueOf(1000));
         veiling.doeBod(BigDecimal.valueOf(2000));
         veiling.doeBod(BigDecimal.valueOf(1000));
