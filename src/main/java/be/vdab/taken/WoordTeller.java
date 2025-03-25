ef1acc195e9441dd171c28a113978c900fb4a553
NickVandenbroeck <nick.vandenbroeck@vdabcampus.be>
2025-03-25 13:54:14 +0100

    woordTeller done

--- src/main/java/be/vdab/taken/WoordTeller.java
+++ src/main/java/be/vdab/taken/WoordTeller.java
@@ -7,6 +7,7 @@ class WoordTeller {
     int telWoorden(String zin) {
         // in delim schrijf je simpelweg alle delimiters achter elkaar tussen "":
         return new StringTokenizer(zin, " ,").countTokens();
+        // dit betekent: splits op spatie en komma - niet op het geheel spatie-en-komma!
     }
 
     //In principe is de Tokenizer verouderd, maar om gewoon woorden te tellen is ie nog ideaal.
