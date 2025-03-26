2d85bfb1715d9604d7071919b8371b8dc96bfc82
NickVandenbroeck <nick.vandenbroeck@vdabcampus.be>
2025-03-26 11:05:57 +0100

    Test JUnit testklasse klaar.

--- src/test/java/be/vdab/test/PersoonTest.java
+++ src/test/java/be/vdab/test/PersoonTest.java
@@ -19,7 +19,7 @@ class PersoonTest {
     @ValueSource(strings = {"Jan", "Piet", "Abdul", "Jean-Pierre", "X Æ A-12", "L", "i", "."})
     void correcteVoornamen(String voornaam) {
         assertThatCode(() -> persoon.add(voornaam)).doesNotThrowAnyException();
-        assertThat(persoon.toString().contains(voornaam)); // gezien de afwezigheid van een getter.
+        assertThat(persoon.toString()).contains(voornaam); // gezien de afwezigheid van een getter.
     }
 
     @ParameterizedTest
@@ -29,14 +29,24 @@ class PersoonTest {
         assertThatIllegalArgumentException().isThrownBy(() -> persoon.add(voornaam));
     }
 
+    @Test
+    void nullVoornaamGeeftNullPointerException() {
+        assertThatNullPointerException().isThrownBy(() -> persoon.add(null));
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"", " ", "   "})
     void legeOfBlancoVoornamen(String voornaam) {
         assertThatIllegalArgumentException().isThrownBy(() -> persoon.add(voornaam));
     }
 
-    @Test
-    void nullVoornaamGeeftNullPointerException() {
-        assertThatNullPointerException().isThrownBy(() -> persoon.add(null));
+    @ParameterizedTest
+    @ValueSource(strings = {"Jan Piet Abdul", "X Æ A-12 Jean-Pierre Abdul Piet Jan", "L i .", "Jan"})
+    void toString(String input) {
+        String[] voornamen = input.split(" "); // split de input string in afzonderlijke namen
+        for (String voornaam : voornamen) {
+            persoon.add(voornaam);
+        }
+        assertThat(persoon.toString()).isEqualTo(input);
     }
 }
